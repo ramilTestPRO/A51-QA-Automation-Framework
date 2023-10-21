fa3c0985ef330e5bcbd728ca1a07e4f15d4fd267
ramilTestPRO <ramil.hasanli@testpro.io>
2023-10-20 20:57:16 -0600

    Update LoginTests.java

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -20,6 +20,6 @@ public class LoginTests extends BaseTest {
         String url = "https://qa.koel.app/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
-        //driver.quit();
+        driver.quit();
     }
 }
