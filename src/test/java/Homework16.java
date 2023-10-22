d44ea0850b21d9778fa3971676afc232a73a0be9
ramilTestPRO <ramil.hasanli@testpro.io>
2023-10-22 17:03:02 -0600

    Create Homework16.java

---
+++ src/test/java/Homework16.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Homework16 extends BaseTest{
+    @Test
+    public void registrationNavigation(){
+        ChromeOptions options= new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        // Set up WebDriver and open the website
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+        WebElement registration = driver.findElement(By.cssSelector("[href='registration']"));
+        registration.click();
+
+        // Verify that  redirected to the Registration page
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertEquals(currentUrl, "https://qa.koel.app/registration");
+
+    }
+}
