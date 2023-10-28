408ccfb04d9f74326eebec94eaf3e51d9f3bcbd1
ramilTestPRO <ramil.hasanli@testpro.io>
2023-10-28 09:02:21 -0600

    First Commit HW18

---
+++ src/test/java/Homework18.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class Homework18 extends BaseTest{
+
+    public void playNextSong() throws InterruptedException {
+        WebElement playButton =driver.findElement(By.xpath("//*[@id=\"mainFooter\"]/div[1]/i[2]"));
+        playButton.click();
+        Thread.sleep(2000);
+
+    }
+       public void isSoundBarDisplayed()  {
+        WebElement soundBarDisplayed = driver.findElement(By.xpath("//*[@id=\"mainFooter\"]/div[2]/div[2]/div/button[1]/div/img"));
+           Assert.assertTrue(soundBarDisplayed.isDisplayed());
+    }
+    @Test
+    public void playSong() throws InterruptedException {
+        // Navigate to "https://qa.koel.app/"
+        navigateToPage();
+        // Log in with your credentials (replace 'your_username' and 'your_password' with your actual credentials)
+        provideEmail("ramil.hasanli@testpro.io");
+        providePassword("iutZVH7Q");
+        clickSubmit();
+        Thread.sleep(2000);
+        playNextSong();
+        isSoundBarDisplayed();
+        //driver.wait();
+
+
+    }
+}
---
+++ src/test/java/LoginTests.java
@@ -0,0 +1,42 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTests extends BaseTest {
+
+    @Test (enabled = false, priority = 2, description = "Login with invalid email and valid password")
+    public void loginInvalidEmailValidPassword() throws InterruptedException {
+
+        navigateToPage();
+        provideEmail("invalid@class.com");
+        providePassword("te$t$tudent");
+        clickSubmit();
+
+        Thread.sleep(2000);
+        Assert.assertEquals(driver.getCurrentUrl(), url); //https://qa.koel.app/
+    }
+
+    @Test (enabled = true, priority = 1, description = "Login with valid email and valid password")
+    public void loginValidEmailPassword(){
+
+        navigateToPage();
+        provideEmail("demo@class.com");
+        providePassword("te$t$tudent");
+        clickSubmit();
+        isAvatarDisplayed();
+
+        Assert.assertEquals(driver.getCurrentUrl(), url); //https://qa.koel.app/
+    }
+
+    @Test (enabled = false, priority = 3, description = "Login with valid email and empty password")
+    public void loginValidEmailEmptyPassword() throws InterruptedException {
+
+        navigateToPage();
+        provideEmail("demo@class.com");
+        providePassword("");
+        clickSubmit();
+
+        Thread.sleep(2000);
+        Assert.assertEquals(driver.getCurrentUrl(), url); //https://qa.koel.app/
+    }
+
+}
