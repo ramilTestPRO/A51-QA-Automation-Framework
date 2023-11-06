0b3ce6702a170df093157bb38bcb6b7a48201b7c
ramilTestPRO <ramil.hasanli@testpro.io>
2023-11-06 15:58:44 -0600

    need to review
    test fails

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,13 +1,38 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 
 public class HomePage extends BasePage{
     public HomePage(WebDriver givenDriver){
         super(givenDriver);
     }
+    By myPlayList = By.xpath("//*[@id=\"playlists\"]/ul/li[3]/a");
+    public void chosePlaylistToRename()  {
+        actions.contextClick(findElement(myPlayList)).perform();
+    }
+    By clickEdit = By.xpath("//*[@id=\"playlists\"]/ul/li[3]/nav/ul/li[1]");
+    public void clickOnEdit()  {
+        findElement(clickEdit).click();
+    }
+By textField = By.cssSelector(" [name = 'name']");
+    public void clearOldName()  {
+       findElement (textField).sendKeys(Keys.chord(Keys.CONTROL, "A", Keys.BACK_SPACE));
+    }
+    By newName = By.cssSelector(" [name = 'name']");
+    public void typeNewName(){
+        findElement(newName).sendKeys("MyNewPlayList", Keys.ENTER);
+    }
+By playlistRenamed = By.cssSelector("div.success.show");
+    public WebElement isPlaylistRenamed()  {
+        return findElement(playlistRenamed);
+           }
+
+
     By userAvatarIcon = By.cssSelector("img.avatar");
     public WebElement getUserAvatar(){
         return findElement(userAvatarIcon);
