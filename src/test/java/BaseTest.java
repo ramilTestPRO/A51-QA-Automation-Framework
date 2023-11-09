efe425240427eea41c87e59fb70d4c15886ace5c
ramilTestPRO <ramil.hasanli@testpro.io>
2023-11-08 21:00:32 -0600

    init

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,18 +4,26 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
 
+import java.lang.reflect.MalformedParametersException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 import java.util.UUID;
 
 public class BaseTest {
-    public WebDriver driver = null;
+    public static WebDriver driver = null;
     public String url = null;
     public WebDriverWait wait = null;
     public Actions actions = null;
@@ -33,18 +41,43 @@ public class BaseTest {
 
     @BeforeMethod
     @Parameters({"BaseURL"})
-    public void launchBrowser(String BaseURL) {
+    public void launchBrowser(String BaseURL) throws MalformedURLException {
         //Added ChromeOptions argument below to fix websocket error
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.manage().window().maximize();
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--remote-allow-origins=*");
+//        driver = new ChromeDriver(options);
+        driver=pickBrowser(System.getProperty("browser"));
+//        driver=new FirefoxDriver();
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//        driver.manage().window().maximize();
         wait = new WebDriverWait(driver,Duration.ofSeconds(10));
         actions=new Actions(driver);
         url=BaseURL;
         navigateToPage();
     }
+    public static WebDriver pickBrowser(String browser){
+        DesiredCapabilities caps=new DesiredCapabilities();
+        String gridURL= "http://10.0.0.206:4444";
+        switch (browser){
+            case "Firefox":
+                WebDriverManager.firefoxdriver().setup();
+                return driver = new FirefoxDriver();
+            case "MicrosoftEdge":
+                WebDriverManager.chromedriver().setup();
+                EdgeOptions edgeOptions=new EdgeOptions();
+                edgeOptions.addArguments("--remote-allow-origins=*");
+                return driver = new EdgeDriver(edgeOptions);
+            case "grid-edge": //gradle clean test -Dbrowser=grid-edge
+                caps.setCapability("browser", "MicrosoftEdge");
+                return driver = new RemoteWebDriver(URL.create(gridURL).toURL().caps);
+            default:
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions options=new ChromeOptions();
+                options.addArguments("--remote-allow-origins=*");
+                return driver = new ChromeDriver(options);
+        }
+    }
+
     @AfterMethod
     public void closeBrowser() {
         driver.quit();
